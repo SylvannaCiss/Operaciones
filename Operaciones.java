d5e703f0e2298b32567fba68def6f4890d723797
SylvannaCiss <20243ds146@utez.edu.mx>
2025-05-14 12:31:14 -0600

    Se agrego division

--- Operaciones.java
+++ Operaciones.java
@@ -4,3 +4,11 @@ public class Operaciones{
 
         String numero1 = JOptionPane.showInputDialog("Ingresa un numero");
         double numeroDecimal1 = Double.parseDouble(numero3);
+
+        String numero2 = JOptionPane.showInputDialog("Ingresa otro numero");
+        double numeroDecimal2 = Double.parseDouble(numero4);
+
+        double division = numeroDecimal1/numeroDecimal2;
+        System.out.println("La division es: "+division);
+
+        
