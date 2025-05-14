0f14b8459df0b0a3f0c43b5c17edf58f88ff1c69
SylvannaCiss <20243ds146@utez.edu.mx>
2025-05-14 12:37:57 -0600

    Se finalizo el codigo

--- Operaciones.java
+++ Operaciones.java
@@ -3,22 +3,26 @@ public class Operaciones{
     public static void main(String[]args){
 
         String numero1 = JOptionPane.showInputDialog("Ingresa un numero");
-        double numeroDecimal1 = Double.parseDouble(numero3);
+        double numeroDecimal1 = Double.parseDouble(numero1);
 
         String numero2 = JOptionPane.showInputDialog("Ingresa otro numero");
-        double numeroDecimal2 = Double.parseDouble(numero4);
+        double numeroDecimal2 = Double.parseDouble(numero2);
 
         double division = numeroDecimal1/numeroDecimal2;
         System.out.println("La division es: "+division);
 
         double suma = numeroDecimal1+numeroDecimal2;
-        System.out.println("La division es: "+suma);
+        System.out.println("La suma es: "+suma);
 
         double resta = numeroDecimal1-numeroDecimal2;
-        System.out.println("La division es: "+resta);
+        System.out.println("La resta es: "+resta);
 
         double multiplicacion = numeroDecimal1*numeroDecimal2;
-        System.out.println("La division es: "+multiplicacion);
+        System.out.println("La multiplicacion es: "+multiplicacion);
+    
+       
+    }
+} 
 
 
 
