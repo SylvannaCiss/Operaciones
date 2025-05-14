62bb15e53664e810540ff5a0cc75806761e40977
SylvannaCiss <20243ds146@utez.edu.mx>
2025-05-14 12:30:50 -0600

    Se agrego division

---
+++ Operaciones.java
@@ -0,0 +1,6 @@
+import javax.swing.JOptionPane;
+public class Operaciones{
+    public static void main(String[]args){
+
+        String numero1 = JOptionPane.showInputDialog("Ingresa un numero");
+        double numeroDecimal1 = Double.parseDouble(numero3);
